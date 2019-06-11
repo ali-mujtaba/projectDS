349eb99a49867d7983c29052a6fd7d14798f111a
Danish Iqbal <iqbaldanish14@gmail.com>
2019-06-10 23:31:59 -0700

    Change java version and add directory structure

---
+++ src/main/java/com/practice/linkedlist/SampleLinkedList.java
@@ -0,0 +1,7 @@
+package com.practice.linkedlist;
+
+public class SampleLinkedList {
+  public static void main(String[] args) {
+    System.out.println("This is a sample linked list class");
+  }
+}
