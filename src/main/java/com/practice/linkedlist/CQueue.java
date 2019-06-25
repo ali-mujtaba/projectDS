2541269c05d102c7454e14ef737942eb24938ae7
ide-ali <am.alimujtaba@gmail.com>
2019-06-25 14:31:18 +0530

    Added: CQueue.java file. Implementation of circular queue using linked list.

---
+++ src/main/java/com/practice/linkedlist/CQueue.java
@@ -0,0 +1,76 @@
+// Implementation of Circular Queue using Linked List
+
+import java.util.*;
+public class CQueue {
+	static Node rear, front;
+	public static void main(String[] args) {
+		Scanner st = new Scanner(System.in);
+		int c = 0;
+		while (c != 4) {
+			System.out.println("\nEnter your choice:-\n 1. Enqueue 2. Dequeue 3. Peek 4. Exit");
+			c = st.nextInt();
+			System.out.println();
+			switch (c) {
+			case 1 : System.out.print("Enter the element to be pushed: ");
+				int d = st.nextInt();
+				enqueue(d);
+				break;
+			case 2 : dequeue();
+				break;
+			case 3 : peek();
+				break;
+			case 4 :
+				break;
+			default : System.out.println("Invalid input! Try Again. ");
+				break;
+			}
+			display();
+		}
+	}
+
+	static void enqueue(int d) {
+		Node node = new Node();
+		node.data = d;
+		if (front == null) {
+			rear = node;
+			front = node;
+		} else {
+			rear.next = node;
+			rear = rear.next;
+			rear.next = front;
+		}
+
+	}
+
+	static void dequeue() {
+		if (front == null) {
+			System.out.println("Queue is empty!");
+		} else {
+			System.out.println("Element removed: " + front.data);
+			if (front == rear)
+				front = rear = null;
+			else
+				front = front.next;
+		}
+	}
+	static void peek() {
+		if (front == null)
+			System.out.println("Queue is empty! ");
+		else
+			System.out.println("Element at front: " + front.data);
+	}
+
+	static void display() {
+		Node curr = new Node();
+		curr = front;
+		System.out.print("Queue: ");
+		if (front != null) {
+			while (curr != rear) {
+				System.out.print(curr.data + " -> ");
+				curr = curr.next;
+			}
+			System.out.print(curr.data);
+		}
+		System.out.println();
+	}
+}
