c6a7845909fa6168ed71d4513a5df87ad91855e0
ali-mujtaba <am.alimujtaba@gmail.com>
2019-09-25 00:04:44 +0530

    Add another implementation to check if list is palindrome

--- src/main/java/com/practice/linkedlist/Palindrome.java
+++ src/main/java/com/practice/linkedlist/Palindrome.java
@@ -10,6 +10,9 @@ public class Palindrome {
 
 		Node head = RemoveDuplicates.seedList();
 
+		NodeContainer container = new NodeContainer(head);
+		System.out.println("List is palindrome: " + checkPalindrome2(head, container));
+
 		Node node = checkPalindrome(head, head);
 
 		System.out.println("List is palindrome! ");
@@ -32,4 +35,33 @@ public class Palindrome {
 		return head.next;
 	}
 
+	public static boolean checkPalindrome2(Node head, NodeContainer currentContainer) {
+		if (currentContainer.reverseNode == null) {
+			return true;
+		}
+
+		int currentReverseNodeData = currentContainer.reverseNode.data;
+		// System.out.println("current reverse node: " + currentReverseNodeData);
+
+		currentContainer.reverseNode = currentContainer.reverseNode.next;
+		if (checkPalindrome2(head, currentContainer)) {
+			if (currentReverseNodeData == currentContainer.forwardNode.data) {
+				// System.out.println("current forward node: " + currentContainer.forwardNode.data);
+				currentContainer.forwardNode = currentContainer.forwardNode.next;
+				return true;
+			}
+		}
+		return false;
+	}
+
+}
+
+class NodeContainer {
+	Node reverseNode;
+	Node forwardNode;
+
+	NodeContainer(Node head) {
+		this.reverseNode = head;
+		this.forwardNode = head;
+	}
 }
